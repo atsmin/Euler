ea6ff5edb2a701ee4150f5c37b1e00b8e030e559
aminami1127 <aminami1127@users.noreply.github.com>
2016-08-31 22:02:18 +0900

    change tabs to spaces

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,25 +1,25 @@
 public class Problem2 {
-	public static void main(String args[]) {
-		int f = 0; 
-		int i = 1;
-		int sum = 0;
-		while (f <= 4E+6) {
-			f = fib(i);
-			if (f % 2 == 0) {
-				sum += f;
-			}
-			i++;
-		}
-		System.out.print(sum);
-	}
-	
-	static int fib(int n) {
-		if (n == 1) {
-			return 1;
-		} else if (n == 2) {
-			return 2;
-		} else {
-			return fib(n - 1) + fib(n - 2);
-		}
-	}
+    public static void main(String args[]) {
+        int f = 0; 
+        int i = 1;
+        int sum = 0;
+        while (f <= 4E+6) {
+            f = fib(i);
+            if (f % 2 == 0) {
+                sum += f;
+            }
+            i++;
+        }
+        System.out.print(sum);
+    }
+    
+    static int fib(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
 }
--- src/Problem3.java
+++ src/Problem3.java
@@ -1,39 +1,39 @@
 import java.util.ArrayList;
 
 public class Problem3 {
-	public static void main(String args[]) {
-		long n = 600851475143L;
-		PrimeCalculator calculator = new PrimeCalculator();
-		ArrayList<Long> factors = calculator.prime_factors(n);
-		System.out.println(factors);
-		System.out.println(factors.get(factors.size() - 1));
-	}
+    public static void main(String args[]) {
+        long n = 600851475143L;
+        PrimeCalculator calculator = new PrimeCalculator();
+        ArrayList<Long> factors = calculator.prime_factors(n);
+        System.out.println(factors);
+        System.out.println(factors.get(factors.size() - 1));
+    }
 }
 
 class PrimeCalculator {
-	public ArrayList<Long> prime_factors(long n) {
-		if (n < 2) {
-			return new ArrayList<Long>();
-		}
-		ArrayList<Long> factors = new ArrayList<>();
-		long p = 2;
-		while (n > 1) {
-			// if p * p is greater than n, there are no primes greater then p.
-			if (p * p > n) {  
-				break;
-			}
-			if (n % p == 0) {
-				// divide n by p completely
-				while (n % p == 0) {
-					n /= p;
-				}
-				factors.add(p);
-			}
-			p++;
-		}
-		if (n > 1) {
-			factors.add((n));
-		}
-		return factors;
-	}
+    public ArrayList<Long> prime_factors(long n) {
+        if (n < 2) {
+            return new ArrayList<Long>();
+        }
+        ArrayList<Long> factors = new ArrayList<>();
+        long p = 2;
+        while (n > 1) {
+            // if p * p is greater than n, there are no primes greater then p.
+            if (p * p > n) {  
+                break;
+            }
+            if (n % p == 0) {
+                // divide n by p completely
+                while (n % p == 0) {
+                    n /= p;
+                }
+                factors.add(p);
+            }
+            p++;
+        }
+        if (n > 1) {
+            factors.add((n));
+        }
+        return factors;
+    }
 }
